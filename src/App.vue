ba2881b6ffc9890b8b3edefa5ce91444144b43e2
dannyp0930 <dannyp0930@gmail.com>
2022-04-17 20:43:43 +0900

    FEAT: products

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="menu">
+    <a v-for="menu in menus" :key="menu">{{ menu }}</a>
+  </div>
+  <div v-for="product in products" :key="product.id">
+    <img :src="product.imgUrl"/>
+    <h4>{{ product.name }}</h4>
+    <p>{{ product.price}} 만원</p>
+    <button @click="increase(product.id)">허위매물신고</button>
+    <span>신고수 : {{ product.report }}</span>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
+  data() {
+    return {
+      menus : ['Home', 'Shop', 'About'],
+      products : [
+          { id: 0, name: '역삼동원룸', price: 60, report: 0, imgUrl: require('./assets/room0.jpg') },
+          { id: 1, name: '천호동원룸', price: 50, report: 0, imgUrl: require('./assets/room1.jpg') },
+          { id: 2, name: '마포구원룸', price: 70, report: 0, imgUrl: require('./assets/room2.jpg') },
+        ],
+    }
+  },
+  methods: {
+    increase(id) {
+      this.products[id].report++;
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -21,6 +41,14 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.menu {
+  background-color: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
